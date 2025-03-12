c427207c9921057be81d783b099616cf811dfde8
aayush0704 <130687281+aayush0704@users.noreply.github.com>
2025-03-13 02:57:39 +0530

    Delete leave_agent.py

--- leave_agent.py
+++
@@ -1,8 +0,0 @@
-def process_leave_request(user_input):
-    """Handles leave requests intelligently"""
-    if "casual leave" in user_input:
-        return "You have 10 casual leaves remaining. Your request is approved."
-    elif "sick leave" in user_input:
-        return "You have 5 sick leaves left. Please upload medical proof."
-    else:
-        return "Specify leave type (Casual/Sick/Vacation)."
